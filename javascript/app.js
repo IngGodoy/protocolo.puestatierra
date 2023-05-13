698447692207002031b4c495a83f382be5585299
Jose Godoy <ing.godoy.jose@gmail.com>
2023-05-13 20:03:16 -0300

    mejoras en la interfaz de usuario

--- javascript/app.js
+++ javascript/app.js
@@ -1,12 +1,12 @@
 const arrayPuestaTierra=[]; // array con la lista puesta a tierra
 let valorMedio=document.getElementById("buttonValorMedio"); // leer el botón de agregar PAT
 let borrar=document.getElementById("buttonBorrar"); // leer el botón borrar array PAT
+let mostrarTabla=document.getElementById("mostrarLista"); 
 let ordenar=document.getElementById("buttonOrdenar"); // leer el botón para ordenar la lista PAT
 let imprimirLista=document.getElementById("buttonImprimir"); // leer el botón para ordenar la lista PAT
 let imprimirTexto=document.getElementById("imprimirDom"); //leer etiqueta del DOM para imprimir
 let form=document.getElementById("form"); // leer formulario
 // funcion para capturar los datos de los objetos puesta a tierra
-
 function crearObjeto(){
     //leer datos del DOM (datos de PAT)
     let nombrePuestaTierra=document.getElementById("nombrePat").value; //Nombre de la puesta a tierra
@@ -19,8 +19,74 @@ function crearObjeto(){
       text:"con nombre "+nombrePuestaTierra+" cargado con exito",
       icon:"success"
     });
-    // alert("Se cargaron los valores de puesta a tierra con nombre: " +nombrePuestaTierra+"  valor:  "+valorPuestaTierra+"de forma exitosa...");
+    crearTablaPat();
 };
+function crearTablaPat() {
+   const tablaPat = document.createElement("table");
+   tablaPat.classList.add("tablePat");
+   tablaPat.innerHTML = `
+   <tr>
+   <th>Número</th>
+   <th>Nombre Puesta a Tierra</th>
+   <th>Valor resistencia a tierra (Ohm)</th>
+   </tr>
+   `;
+   let i = 0;
+   arrayPuestaTierra.forEach((element)=>{
+      let tr = document.createElement("tr");
+      tr.innerHTML = `
+         <td>${i}</td>
+         <td>${element.nombrePuestaTierra}</td>
+         <td>${element.valorPuestaTierra}</td>
+      `;
+      tablaPat.appendChild(tr);
+      i++;
+   });
+   imprimirTexto.appendChild(tablaPat);
+   botonEliminarUno();
+   
+}
+function botonEliminarUno() {
+   let div = document.createElement("div");
+   let label = document.createElement("label");
+   label.innerHTML = "Indique el número de la PAT a eliminar";
+   label.setAttribute("for", "eliminarPat");
+   let input = document.createElement("input");
+   input.setAttribute("type", "text");
+   input.setAttribute("name", "valorPat"); 
+   input.setAttribute("placeholder", "número PAT");
+   input.id="indiceEliminar";
+   let botonEliminar = document.createElement("button");
+   botonEliminar.innerHTML = "Eliminar PAT";
+   botonEliminar.id = "eliminarPat";
+   div.appendChild(label);
+   div.appendChild(input);
+   div.appendChild(botonEliminar);
+   imprimirTexto.appendChild(div);
+   let botonEliminarElemento=document.getElementById("eliminarPat");
+    // evento eliminar un elemento de la lista PAT
+botonEliminarElemento.addEventListener("click", () => {
+   let indice = Number(document.getElementById("indiceEliminar").value); // leer el indice del elemento a eliminar 
+   if (indice >= 0 && indice <= (arrayPuestaTierra.length-1)) {
+      
+      arrayPuestaTierra.splice(indice, 1); // eliminar el elemento de la lista PAT
+      guardarLocalStore(arrayPuestaTierra);
+      reset();
+      swal({
+         title: "Dato eliminado",
+         text: "se eliminó un elemento de la lista de Puesta a tierra",
+         icon: "warning",
+       });
+     
+   } else{
+     swal({
+       title: "Error",
+       text: "Indicar un número valido de puesta a Tierra",
+       icon: "warning",
+     });
+   }
+ }); 
+ };
 function guardarLocalStore(element){
    // guardar la array en el local store cada vez que alla un cambio
    let datosLocalStore=element;
@@ -57,6 +123,13 @@ function borrarArray(array) {
    return array;
 
  }
+ function mensajeArrayVacio(){
+   swal ( {
+      title: "Error",
+      text:"No hay datos que procesar",
+      icon:"warning"
+    });
+ }
 //evento click de agregar puesta a tierra
 form.addEventListener("submit",(element)=>{
     element.preventDefault();
@@ -65,50 +138,78 @@ form.addEventListener("submit",(element)=>{
  });
  // evento valor medio
  valorMedio.addEventListener("click",()=>{
-    let acumulador=0;
-    arrayPuestaTierra.forEach((element)=>{
-        acumulador=element.valorPuestaTierra+acumulador;
-    })
+    if(arrayPuestaTierra.length>0){
+      let acumulador=0;
+       arrayPuestaTierra.forEach((element)=>{
+       acumulador=element.valorPuestaTierra+acumulador;
+       }) 
     let valorMedio=acumulador/arrayPuestaTierra.length;
     let texto= " el valor medio de las resistencias de puesta a tierra es: "+valorMedio.toFixed(1)+" Ohm";
     reset();
     imprimir(texto);
-    
-    
+    } else{
+      swal ( {
+         title: "Error",
+         text:"No hay datos que procesar",
+         icon:"warning"
+       });
+    }   
  });
  // evento borrar todo la lista
  borrar.addEventListener("click",()=>{
-    borrarArray(arrayPuestaTierra);
-    guardarLocalStore(arrayPuestaTierra);
-    reset();
-    swal ( {
-      title: "Datos borrados",
-      text:"Todos los valores de puesta a tierra fueron borrados",
-      icon:"warning"
-    });
+    if(arrayPuestaTierra.length>0){
+      borrarArray(arrayPuestaTierra);
+      guardarLocalStore(arrayPuestaTierra);
+      reset();
+      swal ( {
+        title: "Datos borrados",
+        text:"Todos los valores de puesta a tierra fueron borrados",
+        icon:"warning"
+      });
+    } else{
+      mensajeArrayVacio(); //
+    } 
  });
+ // evento borrar un elemento o mostrar tabla
+ mostrarTabla.addEventListener("click",()=>{
+   if(arrayPuestaTierra.length>0){
+      reset();
+      crearTablaPat();
+   }else{
+      swal ( {
+         title: "Error",
+         text:"No hay datos que procesar",
+         icon:"warning"
+       });
+   }
+});
  // evento ordenar
  ordenar.addEventListener("click",()=>{
-    arrayPuestaTierra.sort((a,b)=>{
-    if (a.valorPuestaTierra>b.valorPuestaTierra){
-       return 1;
+    if(arrayPuestaTierra.length>0){
+      arrayPuestaTierra.sort((a,b)=>{
+         if (a.valorPuestaTierra>b.valorPuestaTierra){
+            return 1;
+         }
+         if (a.valorPuestaTierra<b.valorPuestaTierra){
+             return -1;
+          }
+             return 0;
+         });  
+         reset();
+         let texto="";
+         arrayPuestaTierra.forEach((element)=>{
+           texto= texto+" " +element.nombrePuestaTierra+" ("+element.valorPuestaTierra+"Ohm)  /   ";
+         })
+         imprimir("Orde de las Puesta a tierra en funcón de su resistencia de menor a mayor:");
+         imprimir(texto);
+    } else{
+      mensajeArrayVacio();
     }
-    if (a.valorPuestaTierra<b.valorPuestaTierra){
-        return -1;
-     }
-        return 0;
-    });  
-    reset();
-    let texto="";
-    arrayPuestaTierra.forEach((element)=>{
-      texto= texto+" " +element.nombrePuestaTierra+" ("+element.valorPuestaTierra+"Ohm)  /   ";
-    })
-    imprimir("Orde de las Puesta a tierra en funcón de su resistencia de menor a mayor:");
-    imprimir(texto);
  });
  // evento imprimir
  imprimirLista.addEventListener("click",()=>{
-    const puestaTierraOk= arrayPuestaTierra.filter((element)=>element.valorPuestaTierra<40);
+    if(arrayPuestaTierra.length>0){
+      const puestaTierraOk= arrayPuestaTierra.filter((element)=>element.valorPuestaTierra<40);
     const puestaTierraBad= arrayPuestaTierra.filter((element)=>element.valorPuestaTierra>=40);
     let imprimirOk="";
     let imprimirBad="";
@@ -153,9 +254,14 @@ form.addEventListener("submit",(element)=>{
       }
     }
     
+    }else{
+      mensajeArrayVacio();
+    }
  });
  // evento de carga de página recuperar la array PAT
  document.addEventListener('DOMContentLoaded', ()=> {
    traerLocalStore();
    callApi();
  });
+
+ 
